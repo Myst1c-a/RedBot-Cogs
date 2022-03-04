3d996ecd20df30c10265aba9a9df064f84e002f7
Myst1c-a <95739759+Myst1c-a@users.noreply.github.com>
2022-03-04 15:09:49 +0800

    Add files via upload

---
+++ 8ball/8ball.py
@@ -0,0 +1,32 @@
+from redbot.core import commands
+import random
+import discord   
+
+class EightBall(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    
+    @commands.command(name='8ball', help='Randomly answers your query.')
+    async def eightball(self, ctx,*, question):
+     responses = ['As I see it, yes.',
+             'Yes.',
+             'Positive',
+             'From my point of view, yes',
+             'Convinced.',
+             'Most Likley.',
+             'Chances High',
+             'No.',
+             'Negative.',
+             'Not Convinced.',
+             'Perhaps.',
+             'Not Sure',
+             'Mayby',
+             'I cannot predict now.',
+             'Im to lazy to predict.',
+             'I am tired. *proceeds with sleeping*']
+     response = random.choice(responses)
+     embed=discord.Embed(title="The Magic 8 Ball has Spoken!", color=0xf5fffa)
+     embed.add_field(name='Question: ', value=f'{question}', inline=True)
+     embed.add_field(name='Answer: ', value=f'{response}', inline=False)
+     await ctx.send(embed=embed)
