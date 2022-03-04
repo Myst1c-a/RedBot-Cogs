12eadb784757495fba59b08f62add137af22ecdd
Myst1c-a <95739759+Myst1c-a@users.noreply.github.com>
2022-03-04 12:07:07 +0800

    Add files via upload

---
+++ channels/channels.py
@@ -0,0 +1,21 @@
+from redbot.core import commands
+
+class Channels(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(aliases=['cch'], name='createchannel', help='Creates a channel.') 
+    @commands.has_permissions(manage_channels=True)
+    async def createchannel(self, ctx, channel_name, category : int = None):
+        if category == None:
+            category = None
+        c = self.bot.get_channel(category)
+        guild = ctx.message.guild
+        await guild.create_text_channel(channel_name, category=c)
+        await ctx.send(f'**{channel_name}** channel successfully created.')
+
+    @commands.command(aliases=['cca'], name='createcategory') 
+    @commands.has_permissions(manage_channels=True)
+    async def createcategory(self, ctx, *, category_name):
+     await ctx.guild.create_category(category_name)
+     await ctx.send(f'**{category_name}** category successfully created.')
