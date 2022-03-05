6bb2f0ab0dbd398fa568c78d3a428325f2fc06df
Myst1c-a <95739759+Myst1c-a@users.noreply.github.com>
2022-03-05 13:51:08 +0800

    Update afk.py

--- afk/afk.py
+++ afk/afk.py
@@ -38,13 +38,14 @@ class AFK(commands.Cog):
             await message.reply(f'Welcome back **{message.author.name}**. I have removed your AFK.')
 
         for id, reason in afks.items():
-            member = get(message.guild.members, id=id)
+            member = message.guild.get_member(id)
             if (message.reference and member == (await message.channel.fetch_message(message.reference.message_id)).author) or member.id in message.raw_mentions:
-                emb=discord.Embed(
-                    description=f"{member.mention} is AFK since\n**Message:**\n{reason}",
+                afkembed=discord.Embed(
+                    description=f"{member.mention} is AFK\n**Message:**\n{reason}",
                     color=0xd3c1e3
                 )
                 
             if message.author.bot:
                 return
-            await message.reply(embed=emb)
+                
+            await message.reply(embed=afkembed)
