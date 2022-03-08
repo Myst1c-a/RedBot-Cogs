705244510fc2d50e1bf2d8c7394fac10c9b81deb
Myst1c-a <95739759+Myst1c-a@users.noreply.github.com>
2022-03-08 23:41:39 +0800

    Update afk.py

--- afk/afk.py
+++ afk/afk.py
@@ -71,8 +71,11 @@ class AFK(commands.Cog):
                 
                 meth = afk[f'{message.author.id}']['time']
                 mentionz = afk[f'{message.author.id}']['mentions']
+                value = datetime.datetime.fromtimestamp(meth)
 
-                embed = discord.Embed(title=f"Welcome Back!", description=f"While you were AFK you recieved **{mentionz}** pings.", color=0xd3c1e3)
+                embed = discord.Embed(title=f"Welcome Back!", description=f"While you were AFK you recieved **{mentionz}** pings.", color=0xd3c1e3, timestamp=value)
+                embed.set_footer(text='AFK since')
+                await message.reply(embed=embed)
                 await message.reply(embed=embed)
                 
                 afk[f'{message.author.id}']['AFK'] = 'False'
