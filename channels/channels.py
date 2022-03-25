66c7894fb81d954da823baad8dfb7c6c63798b7a
Myst1c-a <95739759+Myst1c-a@users.noreply.github.com>
2022-03-25 14:24:29 +0800

    Update channels.py

--- channels/channels.py
+++ channels/channels.py
@@ -4,18 +4,43 @@ class Channels(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(aliases=['cch'], name='createchannel', help='Creates a channel.') 
-    @commands.has_permissions(manage_channels=True)
-    async def createchannel(self, ctx, channel_name, category : int = None):
-        if category == None:
-            category = None
+    @commands.group(aliases=['cch'], invoke_without_command=True)
+    async def createchannel(self):
+        pass
+
+    @createchannel.group()
+    async def text(self, ctx, name, category : int = None):
+        """Creates a text channel."""
+        category = category or None
         c = self.bot.get_channel(category)
         guild = ctx.message.guild
-        await guild.create_text_channel(channel_name, category=c)
-        await ctx.send(f'**{channel_name}** channel successfully created.')
+        await guild.create_text_channel(name, category=c)
+        await ctx.send(f'**{name}** channel successfully created.')
 
-    @commands.command(aliases=['cca'], name='createcategory', help='Creates a category.') 
+
+    @createchannel.group()
+    async def voice(self, ctx, name, category : int = None):
+        """Creates a voice channel."""
+        category = category or None
+        c = self.bot.get_channel(category)
+        guild = ctx.message.guild
+        await guild.create_voice_channel(name, category=c)
+        await ctx.send(f'**{name}** channel successfully created.')
+
+
+    @createchannel.group()
+    async def stage(self, ctx, name, category : int = None):
+        """Creates a stage channel."""
+        category = category or None
+        c = self.bot.get_channel(category)
+        guild = ctx.message.guild
+        await guild.create_stage_channel(name, category=c)
+        await ctx.send(f'**{name}** channel successfully created.')
+
+
+    @commands.command(aliases=['cca'], name='createcategory') 
     @commands.has_permissions(manage_channels=True)
     async def createcategory(self, ctx, *, category_name):
+        """"Creates a category channel."""
      await ctx.guild.create_category(category_name)
      await ctx.send(f'**{category_name}** category successfully created.')
