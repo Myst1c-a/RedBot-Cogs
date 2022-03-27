731919005b02d0503c0cd4b9875019f81644de79
Myst1c-a <95739759+Myst1c-a@users.noreply.github.com>
2022-03-27 23:53:36 +0800

    Update screenshot.py

--- screenshot/screenshot.py
+++ screenshot/screenshot.py
@@ -28,8 +28,7 @@ class Screenshot(commands.Cog):
     @screenshot.command()
     async def send(self, ctx, channel : discord.TextChannel, content : str):
         """Sends a discord attachment into a channel."""
-        e=discord.Embed(color=0xd3c1e3)
-        e.description(content)
+        e=discord.Embed(description=content, color=0xd3c1e3)
         e.set_image(url=ctx.message.attachments[0].url)
         await channel.send(embed=e)
         await ctx.send('Successfully sent the attachment.')
