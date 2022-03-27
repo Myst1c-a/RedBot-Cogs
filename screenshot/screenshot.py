9cb5bf08b7ddbbe263e11e3769bb6e8b12f7f2e9
Myst1c-a <95739759+Myst1c-a@users.noreply.github.com>
2022-03-27 23:05:03 +0800

    Update screenshot.py

--- screenshot/screenshot.py
+++ screenshot/screenshot.py
@@ -11,12 +11,26 @@ class Screenshot(commands.Cog):
         self.bot = bot
 
 
-    @commands.command(aliases=['ss'])
+    @commands.group(aliases=['ss'])
     async def screenshot(self, ctx, site):
-        """Returns a screenshot of a site, this may not be accurate."""
+        """Screenshot related commands."""
+        
+    @screenshot.command()
+    async def link(self, ctx, site):
+        """Converts a URL into a embedded image."""
         if not re.fullmatch(URL_REGEX, site):
             await ctx.send('Invalid URL')
             return
         e=discord.Embed(color=0xd3c1e3, timestamp=ctx.message.created_at)
         e.set_image(url=f"https://api.popcat.xyz/screenshot?url={site}")
         await ctx.send(embed=e)
+        
+        
+        
+    @screenshot.command()
+    async def send(self, ctx, channel : discord.TextChannel = None):
+        if channel is None:
+            await ctx.send('No channel provided.')
+        else:
+            await channel.send(ctx.message.attachments[0].url)
+            await ctx.send(f'Successfully sent attachment.')
