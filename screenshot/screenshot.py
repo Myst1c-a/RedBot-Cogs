2a91a218924c136b94603731b199117bac9dc9ef
Myst1c-a <95739759+Myst1c-a@users.noreply.github.com>
2022-03-31 12:33:52 +0800

    Update screenshot.py

--- screenshot/screenshot.py
+++ screenshot/screenshot.py
@@ -22,7 +22,7 @@ class Screenshot(commands.Cog):
         if not re.fullmatch(URL_REGEX, site):
             await ctx.send('Invalid URL')
             return
-        e=discord.Embed(color=0xd3c1e3, timestamp=ctx.message.created_at)
+        e=discord.Embed(color=self.color, timestamp=ctx.message.created_at)
         e.set_image(url=f"https://api.popcat.xyz/screenshot?url={site}")
         await ctx.send(embed=e)
         
@@ -30,7 +30,7 @@ class Screenshot(commands.Cog):
     async def send(self, ctx, channel : discord.TextChannel, *, content = None):
         """Sends a discord attachment into a channel."""
         if content == None:
-            e=discord.Embed(color=0xd3c1e3)
+            e=discord.Embed(color=self.color)
             e.set_image(url=ctx.message.attachments[0].url)
             await channel.send(embed=e)
             await ctx.message.add_reaction('✅')
@@ -38,7 +38,7 @@ class Screenshot(commands.Cog):
             await ctx.message.delete()
             return
         else:
-            e=discord.Embed(description=content, color=0xd3c1e3)
+            e=discord.Embed(description=content, color=self.color)
             e.set_image(url=ctx.message.attachments[0].url)
             await channel.send(embed=e)
             await ctx.message.add_reaction('✅')
